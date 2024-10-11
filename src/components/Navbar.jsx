4644bfdc27fbcb128c2e7826efeaa20f6d2bc46b
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-10-11 15:53:49 +0200

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,9 @@ function Navbar(){
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/todolist">ToDoList</Link></li>
                 <li><Link to="/505055">505055</Link></li>
+                <li><Link to="/greeting">greeting</Link></li>
+                <li><Link to="/count">count</Link></li>
+                <li><Link to="/register">register</Link></li>
             </ul>
         </nav>
         <hr />
